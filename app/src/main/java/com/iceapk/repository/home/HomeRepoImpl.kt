d74a9900c4b2798bbde7a5c4b9c1f36691e32485
Adewole Opeyemi <adewoleopw@gmail.com>
2022-08-26 20:51:04 +0100

    Setting up home di

--- app/src/main/java/com/iceapk/repository/home/HomeRepoImpl.kt
+++ app/src/main/java/com/iceapk/repository/home/HomeRepoImpl.kt
@@ -1,4 +1,19 @@
 package com.iceapk.repository.home
 
-class HomeRepoImpl: HomeRepo {
+import com.iceapk.network.interfaces.ICEService
+import com.iceapk.presentation.data.models.Product
+import javax.inject.Inject
+import javax.inject.Named
+
+class HomeRepoImpl
+    @Inject constructor(@Named("ICE") private val service: ICEService): HomeRepo {
+    override suspend fun getAllProducts(): List<Product> {
+        val productsDTO = service.getCart().products
+        val products = arrayListOf<Product>()
+        productsDTO.map {
+            val product = Product(it.id, it.quantity)
+            products.add(product)
+        }
+        return products
+    }
 }
